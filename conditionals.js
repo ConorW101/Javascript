f60c1580a9a1246ab9c7e86f9f84ac4b5eaa05de
conor.wiltshire1992 <conor.wiltshire1992@gmail.com>
2022-02-01 14:31:56 +0000

    conditionals excercise

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+//let age = 8;
+//let age = 25;
+let age = 75;
+
+
+if (age >= 18 && age <= 65) {
+    console.log("between th age of 18 & 65");
+  } else if (age < 18) {
+    console.log("Under the age of 18");
+  } else {
+    console.log("above the age of 65");
+  }
+
+let over50 = age >= 50 ? "50 or over" : "Under 50";
+console.log(over50);
