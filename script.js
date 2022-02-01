20d00e93cd0faec878d7a11664a4115105265ec7
conor.wiltshire1992 <conor.wiltshire1992@gmail.com>
2022-02-01 10:32:59 +0000

    Javascript excercises

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+console.log("Conor");
+
+console.info("Wiltshire");
+
+console.warn("Watford");
+
+console.error("Leo");
+
+let carMake = "Aston Martin";
+
+let carModel = "DB11"
+
+console.log("my favourite car is:" + carMake + " and the model is: " + carModel);
+
+console.log(`my favourite car is ${carMake} and the model is ${carModel}`);
+
+console.log("my favourite car is: %c "+ carMake + " and the model is: " + carModel ,"color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
